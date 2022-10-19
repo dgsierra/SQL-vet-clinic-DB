20ea0513676baf2d68818f0108a06b8c4011e87c
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-10-18 23:33:34 -0500

    Add schema create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
-/* Database schema to keep the structure of entire database. */
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id bigint NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    scape_attempts bigint,
+    neutered boolean NOT NULL,
+    weight_kg numeric,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
