5d09f0dcb06b8fd637e2d216ce6fb0316af3e964
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-10-19 02:16:15 -0500

    Fix typo in data.sql

--- data.sql
+++ data.sql
@@ -5,5 +5,4 @@ INSERT INTO public.animals(
     (1, 'Agumon', '02-03-2020', 0, true, 10.23),
     (2, 'Gabumon', '11-15-2018', 2, true, 8);,
     (3, 'Pikachu', '01-07-2021', 1, false, 15.04),
-    (4, 'Devimon', '05-12-2017', 5, true, 11),
     (4, 'Devimon', '05-12-2017', 5, true, 11);
