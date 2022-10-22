1dd4bac6b95809956c8ba2486cd8cd68e9961626
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-10-22 18:18:54 -0500

    Create tables

--- schema.sql
+++ schema.sql
@@ -37,3 +37,31 @@ ALTER TABLE animals ADD species varchar(100);
 ALTER TABLE animals ADD species_id BIGINt REFERENCES species(id);
 
 ALTER TABLE animals ADD owners_id BIGINT REFERENCES owners(id);
+
+ALTER TABLE animals ADD species_id BIGINT REFERENCES species(id);
+
+ALTER TABLE animals ADD owners_id BIGINT REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specialization (
+    vets_id BIGINT REFERENCES vets (id),
+    species_id BIGINT REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+    vets_id BIGINT REFERENCES vets (id),
+    animals_id BIGINT REFERENCES animals (id),
+    date_of_visit DATE NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species varchar(100);
+
+ALTER TABLE animals ADD species_id BIGINT REFERENCES species(id);
